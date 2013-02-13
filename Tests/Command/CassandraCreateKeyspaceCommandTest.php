724be0d1399c240d3163874b9fd8c175000af4df
Ricard Clau <ricard.clau@gmail.com>
2013-02-13 20:08:11 +0100

    add travis and first test

--- Tests/Command/CassandraCreateKeyspaceCommandTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace ADR\Bundle\CassandraBundle\Tests\Command;
-
-class CassandraCreateKeyspaceCommandTest
-{
-
-}
---
+++ Tests/DependencyInjection/ADRCassandraExtensionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ADR\Bundle\CassandraBundle\Tests\DependencyInjection;
+
+use ADR\Bundle\CassandraBundle\DependencyInjection\ADRCassandraExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ADRCassandraExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \ADR\Bundle\CassandraBundle\DependencyInjection\ADRCassandraExtension
+     */
+    private $extension;
+
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerBuilder
+     */
+    private $container;
+
+    protected function setUp()
+    {
+        $this->extension = new ADRCassandraExtension();
+        $this->container = new ContainerBuilder();
+    }
+
+    protected function tearDown()
+    {
+        $this->extension = null;
+        $this->container = null;
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     * @expectedExceptionMessage child node "clusters"
+     */
+    public function testEmptyConfigThrowsException()
+    {
+        $this->extension->load(array(), $this->container);
+    }
+}
